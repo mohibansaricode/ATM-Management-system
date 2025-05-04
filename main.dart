9476b998a81312b740782c6f03625047d9437ecc
mohibansaricode <mmcode46@gmail.com>
2025-05-04 14:09:08 +0500

    4th Assigment Starting a atm management system

---
+++ main.dart
@@ -0,0 +1,15 @@
+void main() {
+  
+// ASSIGNMENT 04 DART PROJECT
+// For this assignment, you are required to create a console-based application using Dart, demonstrating your understanding of fundamental programming concepts, including lists, maps, loops, and functions. The goal of this project is to test your ability to apply these concepts effectively in a practical application.
+
+
+// Console Based ATM Management System
+
+
+
+
+
+
+
+}
